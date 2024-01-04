80d22610a35cbe594a1d3ea7e66b06cd106037cc
TheQueen125 <155658559+TheQueen125@users.noreply.github.com>
2024-01-04 23:00:02 +0530

    Add files via upload

---
+++ else1.java
@@ -0,0 +1,23 @@
+class else1
+{
+ public static void main(String[]args)
+  {
+    int a=20;
+	int b=80;
+	int c=5;
+	if(a>c && a>b)
+	 {
+	  System.out.println("A is greatest");
+	  }
+	  if(b>a && b>c)
+	   {
+	    System.out.println("B is gratest");
+		}
+		else
+		{
+		  System.out.println("C is gratest");
+		  }
+  }
+}
+		 
+		 
